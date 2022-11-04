8eef45c4b037923fd8ceb3e38fe6b39a29b166cf
Woan-Nu Yao <woannu.y@gmail.com>
2022-11-03 18:06:44 -0700

    restructure html, css and add more DOM element in javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,32 @@
 //set DOM element HOOK
 
-var citySearchEl = document.querySelector("#city");
+var citySearchEl = document.querySelector("#input-city");
 var weatherResultsEl = document.querySelector(".container-result");
 var searchHistoryEl = document.querySelector(".search-history");
 var currentWeatherEl = document.querySelector("#current-weather");
 var fiveDayEl = document.querySelector("#five-day-weather");
+var searchBtnEl = document.querySelector(".search-btn");
+var inputCityEl = document.querySelector("#input-city");
+var searchResultEl = document.querySelector("#search-result");
+var currentCityEl = document.querySelector("#current-city");
+var tempEl = document.querySelector("#temp");
+var windEl = document.querySelector("#wind-speed");
+var humidityEl = document.querySelector("#humidity");
 
-var APIkey = "abbf600e22d96772d150b6ff095a932f"
-var baseURL ="https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=abbf600e22d96772d150b6ff095a932f"
+
+
+
 //function to make API calls
-
+var getCityWeather = function (city){
+    //var apiKey = "abbf600e22d96772d150b6ff095a932f";
+    var baseURL ='https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=abbf600e22d96772d150b6ff095a932f';
 fetch(baseURL)
 .then(function (response) {
-    return response.json();
+    console.log(response);
 })
 .then(function (data) {
-    console.log(data);
+   displayWeather(data,city);
+   console.log(data);
   });
+};
+
